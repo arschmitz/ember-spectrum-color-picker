78b4627081317f2b3664531a34a83084a6e2ff48
Alexander Schmitz <arschmitz@gmail.com>
2017-08-25 00:33:18 -0400

    Update fastboot support

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 /* eslint-env node */
 'use strict';
 
-var path = require('path');
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
-
-var defaults = {
-  assetPath: 'vendor'
-};
-
 module.exports = {
   name: 'ember-spectrum-color-picker',
+
+  options: {
+    nodeAssets: {
+      'spectrum-colorpicker': {
+        vendor: ['spectrum.js', 'spectrum.css'],
+        processTree(input) {
+          return fastbootTransform(input);
+        }
+      }
+    }
+  },
 
   included: function(app) {
     this._super.included(app);
 
-    var options = (app && app.project.config(app.env)['emberSpectrumColorPicker']) || {};
-
-    var assetPath = options.assetPath || defaults.assetPath;
-
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(path.join(assetPath, 'spectrum.js'));
-    }
-
-    app.import(path.join(assetPath, 'spectrum.css'));
-  },
-
-  treeForVendor(vendorTree) {
-    var spectrumTree = new Funnel(path.dirname(require.resolve('spectrum-colorpicker')), {
-      files: ['spectrum.js', 'spectrum.css']
-    });
-
-    return vendorTree ? mergeTrees([vendorTree, spectrumTree]) : spectrumTree;
+    app.import('vendor/spectrum-colorpicker/spectrum.js');
+    app.import('vendor/spectrum-colorpicker/spectrum.css');
   }
 };
